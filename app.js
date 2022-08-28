b5ebb7bd2c91d26bf5d0bd031646aad6f8b8ce00
Diego Pagini <diego.pagini@neoris.com>
2022-08-28 21:14:47 +0200

    console

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const { createFile } = require('./helpers/multiply');
 
 console.clear(); // To clear the console.
 
-const base = 5;
+console.log(process.argv); // This will show info in the console about path and params. The third param is the argument.
+const [, , arg3 = 'base=1'] = process.argv;
+const [, base = 1] = arg3.split('='); // In this way we can get the param sent in the console.
 
 createFile(base)
 	.then((fileName) => console.log(`${fileName} created`))
