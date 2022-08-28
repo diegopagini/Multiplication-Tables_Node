13d6e8ba9c3bfbd6be83c33956f479d87416d4ac
Diego Pagini <diego.pagini@neoris.com>
2022-08-28 19:58:42 +0200

    create a system file

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.clear()
+/** @format */
+
+const fs = require('fs'); // "fs" is from File System. And "require" is the way to import packages in node.
+
+console.clear(); // To clear the console.
+
+// console.log('====================');
+// console.log('   Tabla del: 5');
+// console.log('====================');
+
+const base = 5;
+let salida = '';
+
+for (let i = 1; i <= 10; i++) {
+	salida += `${base} x ${i} = ${base * i}\n`;
+}
+
+fs.writeFileSync('tabla-5.txt', salida, (err) => {
+	// fs.writeFileSync to create a file
+	if (err) throw err;
+
+	console.log('tabla-5.txt creado');
+});
